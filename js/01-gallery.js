0aca4e2e012dab78f3a3b7557b615c298d14b89d
Vitaliy Zhoglo <zhoglo.vitaliy@gmail.com>
2022-05-29 23:01:15 +0300

    1 task - 1!done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,36 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+console.log(createGalleryEl(galleryItems));
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryEl = createGalleryEl(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryEl);
+
+galleryContainer.addEventListener('click', onGalleryElClick);
+
+function createGalleryEl(pictures) {
+    return pictures.map(({ preview, original, description }) => {
+        return `
+        <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+        </div>`;
+    }).join('');
+};
+
+function onGalleryElClick (evt) {
+    // console.log(evt.target);
+    if (!evt.target.classList.contains('gallery__image')) {
+        return;
+    }
+}
