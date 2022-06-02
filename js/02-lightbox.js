eb79a8ea0257d7bddee07e3cd5b651db83219cd3
Vitaliy Zhoglo <zhoglo.vitaliy@gmail.com>
2022-06-02 22:03:51 +0300

    2task - done!

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,23 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryEl = createGalleryEl(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryEl);
+
+function createGalleryEl(pictures) {
+  return pictures
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" /></a>`;
+    })
+    .join('');
+}
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
